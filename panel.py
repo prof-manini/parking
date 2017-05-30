cef1f28638b9b6a73dd7de0dd9ce212f8003e9c8
luca manini <prof.manini@gmail.com>
2017-05-30 13:47:50 +0200

    Fix missing def of "info" (logging), minor glitches and comments.

--- panel.py
+++ panel.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 
-# File panel.py contenente la classe panel (pannello posti liberi), il metodo
-# main e le classi per la gestione della comunicazione tra processi via socket.
+# File panel.py contenente la classe panel (pannello posti liberi), il
+# metodo main e le classi per la gestione della comunicazione tra
+# processi via socket.
+
 import pygame
 import random
-import os
-import argparse
 import settings as opt
 import logging
 import sys
 import time
 
+info = logging.info
+
 # eventuali globali
 WIDTH = 600
 HEIGHT = 400
-TITLE = "Pannello posti liberi"
+WINDOW_TITLE = "Pannello posti liberi"
 THICKNESS = 2
 
-#******************************************************
+# ******************************************************
 
 import socket
 class PanelSocketServer():
@@ -67,7 +69,6 @@ class PanelSocketClient():
         self.sock.close()
         return data
 
-
 #*************************************************************
 # classe Panel
 class Panel:
@@ -75,18 +76,21 @@ class Panel:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption(TITLE)
+        pygame.display.set_caption(WINDOW_TITLE)
         self.running = True
+        self.header = "PARCHEGGIO ISTITUTO DI ISTRUZIONE LA ROSA BIANCA"
         self.footer = "CAVALESE - "
         self.time = time.strftime("%H:%M")
-        self.zone1 = list() #lista compredente due elementi, il primo è il numero di posti liberi ed il secondo è la stringa contenente il colore
-        self.zone2 = list()
-        self.zone3 = list()
+
+        # coppie (numero di posti liberi, stringa colore)
+
+        self.zone1 = list((10, "WHITE"))
+        self.zone2 = list((20, "RED"))
+        self.zone3 = list(( 5, "GREEN"))
+
         with open("port_panel.txt") as file:
             self.port = int(file.read())
         self.client = PanelSocketClient(port=self.port)
-
-
 
     def events(self):
         for event in pygame.event.get():
@@ -104,26 +108,36 @@ class Panel:
        #work with information
 
     def draw(self):
-        self.screen.fill(opt.LIGHTBLUE)
+
         pygame.font.init()
         self.headerFont = pygame.font.SysFont('Times New Roman', 20)
-        self.headerSurface = self.headerFont.render("PARCHEGGIO ISTITUTO D'ISTRUZIONE LA ROSA BIANCA", False, opt.WHITE)
-        self.screen.blit(self.headerSurface,(35,25))
-        pygame.draw.lines(self.screen, opt.WHITE, False, [(0,70),(WIDTH,70)], THICKNESS)
-        pygame.draw.lines(self.screen, opt.WHITE, False, [(0,HEIGHT - 70),(WIDTH,HEIGHT -70)], THICKNESS)
         self.zoneFont = pygame.font.SysFont('Comics Sans', 32)
-        self.screen.blit(self.zoneFont.render("ZONA 1", False, opt.WHITE), (110, 135))
-        self.screen.blit(self.zoneFont.render("ZONA 2", False, opt.WHITE), (110, 190))
-        self.screen.blit(self.zoneFont.render("ZONA 3", False, opt.WHITE), (110, 245))
+
+        blit = self.screen.blit
+        lines = pygame.draw.lines
+
+        self.screen.fill(opt.LIGHTBLUE)
+        self.headerSurface = self.headerFont.render(self.header, False, opt.WHITE)
+        blit(self.headerSurface,(35,25))
+        lines(self.screen, opt.WHITE, False, [(0,70),(WIDTH,70)], THICKNESS)
+        lines(self.screen, opt.WHITE, False, [(0,HEIGHT - 70),(WIDTH,HEIGHT -70)], THICKNESS)
+        render = self.zoneFont.render
+        blit(render("ZONA 1", False, opt.WHITE), (110, 135))
+        blit(render("ZONA 2", False, opt.WHITE), (110, 190))
+        blit(render("ZONA 3", False, opt.WHITE), (110, 245))
 
         self.freePlaces = pygame.font.SysFont('Comics Sans', 32)
 
-        # nel primo parametro della funzione render andrà messo self.zone[0], il 5 è stato messo solo di   prova per testare il codice scritto.
-        #self.screen.blit(self.freePlaces.render("         %s         " % (self.zone1[0]), False,(255,128,0),opt.BLACK), (230,135))
-        #self.screen.blit(self.freePlaces.render("         %s         " % (self.zone2[0]), False, (255,128,0),opt.BLACK), (230,190))
-        #self.screen.blit(self.freePlaces.render("         %s         " % (self.zone3[0]), False, (255,128,0),opt.BLACK), (230,245))
+        fmt = "         %s         "
+        render = self.freePlaces.render
+        blit(render(fmt % (self.zone1[0]), False, (255,128,0), opt.BLACK), (230,135))
+        blit(render(fmt % (self.zone2[0]), False, (255,128,0), opt.BLACK), (230,190))
+        blit(render(fmt % (self.zone3[0]), False, (255,128,0), opt.BLACK), (230,245))
 
-        # Anche qui i colori sono stati inseriti per provare il codice. Nel programma vero dovranno cambiare in base al numero di posti liberi.
+        # Anche qui i colori sono stati inseriti per provare il
+        # codice.  Nel programma vero dovranno cambiare in base al
+        # numero di posti liberi.
+
         pygame.draw.circle(self.screen, opt.GREEN, (450,145), 12, 0)
         pygame.draw.circle(self.screen, opt.GREEN, (450,200), 12, 0)
         pygame.draw.circle(self.screen, opt.RED, (450,255), 12, 0)
@@ -139,13 +153,9 @@ class Panel:
             self.update()
             self.draw()
 
-
-
-# main
 def main():
     p = Panel()
     p.run()
-
 
 if __name__ == "__main__":
     sys.exit(main())
